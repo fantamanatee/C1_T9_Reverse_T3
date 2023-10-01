bbc3decf16ff51c385da470f24eca964b38fd8df
Mario Morelos <mmorelos2@tamu.edu>
2023-10-01 16:37:27 -0500

    Partially filled out main

--- src/Crawler_rev.cpp
+++ src/Crawler_rev.cpp
@@ -1,5 +1,54 @@
 #include "Datatable.h"
 #include "Parser.h"
-int main() {
-    return 0;
+#include <iostream>
+#include <fstream>
+#include <curl/curl.h>
+
+int main(int argc, char *argv[])
+{
+    long long fstream_options;
+
+    if (argc == 2)
+    {
+        std::fstream myFile;
+        myFile.open(argv[1], std::ios::in | std::ios::out);
+        if (myFile.is_open())
+        {
+            // Instantiate datatable
+            std::string myText;
+            while (getline(myFile, myText))
+            {
+                if (4 < myText.size())
+                {
+                    std::string url = myText.c_str();
+                    int sender_success_code = sender(); // Fill in args
+                    if (sender_success_code == 0)
+                    {
+                        // Random copy paste of url = cstr???
+                        printf("HTTP request succeeded for URL: %s\n", url);
+                    }
+                    else
+                    {
+                        // Random copy paste of url = cstr???
+                        printf("HTTP request failed for URL: %s\n", url);
+                    }
+                }
+            }
+            fstream_options = 0;
+            // deconstruct string object
+            // deconstruct datatable object
+        }
+        else
+        {
+            puts("Failed to open file");
+            fstream_options = 1;
+        }
+    }
+    else
+    {
+        puts("give url file please");
+        fstream_options = 1;
+    }
+
+    return fstream_options;
 }
--- src/Datatable.h
+++ src/Datatable.h
@@ -5,17 +5,28 @@
 class Datatable {
 public:
     Datatable();
-    void saveWebsite();
+    void saveWebsite(char *param_1, char *param_2);
     ~Datatable();
 };
 
 Datatable::Datatable() {
-
+    /*
+    *(int *)(this + 0x190004) = 0;
+    *(int *)(this + 0x190008) = 0x4000;
+    *(int *)(this + 0x190000) = 100;
+    *(int *)(this + 0x19000c) = 100;
+    */
+    int a = 100;
+    int b = 0;
+    int c = 0x4000;
+    int d = 100;
 }
+
 Datatable::~Datatable() {
 
 }
-void Datatable::saveWebsite() {
+
+void Datatable::saveWebsite(char *param_1, char *param_2) {
 
 }
 
