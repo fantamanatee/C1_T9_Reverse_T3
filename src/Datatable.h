86ca263775627869eddfb8b5ad5760cf04d34236
Mario Morelos <mmorelos2@tamu.edu>
2023-10-03 17:14:12 -0500

    Almost finished source code

--- src/Datatable.h
+++ src/Datatable.h
@@ -1,33 +1,39 @@
 #ifndef DATATABLE_H
 #define DATATABLE_H
-
+#include <cstdio>
+#include <cstring>
 
 class Datatable {
 public:
+    int a;
+    int websites_saved;
+    int c;
+    int d;
+
     Datatable();
     void saveWebsite(char *content, char *userdata);
     ~Datatable();
 };
 
-Datatable::Datatable() {
-    /* How do I recreate this? This def hiding something I can feel it
-    *(int *)(this + 0x190004) = 0;
-    *(int *)(this + 0x190008) = 0x4000;
-    *(int *)(this + 0x190000) = 100;
-    *(int *)(this + 0x19000c) = 100;
-    */
-    int a = 100;
-    int b = 0;
-    int c = 0x4000;
-    int d = 100;
-}
+int HashFunction(char *content, Datatable *datatable, int param_3);
+
+Datatable::Datatable() : a(100), websites_saved(0), c(0x4000), d(100) {}
 
 Datatable::~Datatable() {
 
 }
 
 void Datatable::saveWebsite(char *content, char *userdata) {
-	// ???	
+	unsigned int a = HashFunction(content, this, this->c);
+    printf("%s|||-%d-|||\n", content, (unsigned long) a);
+    strcpy((char *)(this + (long)(this->websites_saved * this->d) + 0x10), content); // The asm instr adds a random (char *)(our_calc) * 0x10 in param 1
+                                                                                     // of strcpy which must be removed
+    strcpy((char*)(this + a), userdata);
+    this->websites_saved += 1;
+}
+
+int HashFunction(char *content, Datatable *datatable, int param_3) {
+    return datatable->websites_saved * datatable->c;
 }
 
 #endif DATATABLE_H
