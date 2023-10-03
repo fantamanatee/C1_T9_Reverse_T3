f33cfe38cceecad86388619083665b5a4e4f825f
Mario Morelos <mmorelos2@tamu.edu>
2023-10-02 19:21:37 -0500

    Changed datatable allocation from heap to stack

--- src/Crawler_rev.cpp
+++ src/Crawler_rev.cpp
@@ -5,14 +5,13 @@
 #include <curl/curl.h>
 
 unsigned long write_callback(void *content, unsigned long size, unsigned long nmemb, void *userdata) {
-    //Datatable::saveWebsite(*(Datatable **)((long)userdata + 8), (char *)userdata, (char *)content);
-    // How tf am I supposed to call savewebsite???
+    (*(Datatable **)((long)userdata + 8))->saveWebsite((char *)userdata, (char *)content);
     printf("%d\n", size * nmemb & 0xffffffff);
     return size * nmemb;
 }
 
 long sender(char *url, Datatable *datatable) {
-    curl_global_init(3); // What is param??? must be == 3
+    curl_global_init(3);
     CURL* curl = curl_easy_init();
     if (curl != 0) {
         curl_easy_setopt(curl, CURLOPT_URL, url);
@@ -39,14 +38,14 @@ int main(int argc, char *argv[])
         myFile.open(argv[1], std::ios::in | std::ios::out);
         if (myFile.is_open() == '\x01')
         {
-            Datatable *datatable = new Datatable();
+            Datatable datatable;
             std::string myText;
             while (getline(myFile, myText))
             {
                 if (4 < myText.size())
                 {
                     char *url = (char *) myText.c_str();
-                    int sender_success_code = sender(url, datatable);
+                    int sender_success_code = sender(url, &datatable);
                     if (sender_success_code == 0)
                     {
                         char *url = (char *) myText.c_str();
@@ -60,8 +59,6 @@ int main(int argc, char *argv[])
                 }
             }
             fstream_options = 0;
-            // deconstruct string object
-            // deconstruct datatable object
         }
         else
         {
