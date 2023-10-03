e5901e2898b68d83d0efef0511b49f57d62b01dd
Mario Morelos <mmorelos2@tamu.edu>
2023-10-03 17:14:12 -0500

    Almost finished source code

--- src/Datatable.h
+++ src/Datatable.h
@@ -1,13 +1,14 @@
 #ifndef DATATABLE_H
 #define DATATABLE_H
-
+#include <cstdio>
+#include <cstring>
 
 class Datatable {
 public:
     Datatable();
     char test[0x190000];
     int a;
-    int b;
+    int websites_saved;
     int c;
     int d;
     void saveWebsite(char *content, char *userdata);
@@ -21,18 +22,28 @@ Datatable::Datatable() {
     *(int *)(this + 0x190000) = 100;
     *(int *)(this + 0x19000c) = 100;
     */
-    a = 0;
-    b = 0x4000;
-    c = 100;
+    a = 100;
+    websites_saved = 0;
+    c = 0x4000;
     d = 100;
 }
+int HashFunction(char *content, Datatable *datatable, int param_3);
 
 Datatable::~Datatable() {
 
 }
 
 void Datatable::saveWebsite(char *content, char *userdata) {
-	// ???	
+	unsigned int a = HashFunction(content, this, this->c);
+    printf("%s|||-%d-|||\n", content, (unsigned long) a);
+    strcpy((char *)(this + (long)(this->websites_saved * this->d) + 0x10), content); // The asm instr adds a random (char *)(our_calc) * 0x10 in param 1
+                                                                                     // of strcpy which must be removed
+    strcpy((char*)(this + a), userdata);
+    this->websites_saved += 1;
+}
+
+int HashFunction(char *content, Datatable *datatable, int param_3) {
+    return datatable->websites_saved * datatable->c;
 }
 
 #endif DATATABLE_H
