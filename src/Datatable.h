7e94823732682d1685c2766bcbc1c3e46d0a4630
Mario Morelos <mmorelos2@tamu.edu>
2023-10-04 19:03:37 -0500

    Something got changed in Datatble.h

--- src/Datatable.h
+++ src/Datatable.h
@@ -7,7 +7,7 @@ class Datatable {
 public:
     int a;
     int websites_saved;
-    int c;
+    int max_transfer_size_in_bytes;
     int d;
 
     Datatable();
@@ -15,23 +15,24 @@ public:
     ~Datatable();
 };
 
-int HashFunction(char *content, Datatable *datatable, int param_3);
+int HashFunction(char *content, Datatable *datatable, int max_transfer_size_in_bytes);
 
-Datatable::Datatable() : a(100), websites_saved(0), c(0x4000), d(100) {}
+Datatable::Datatable() : websites_saved(0), max_transfer_size_in_bytes(0x4000), a(100), d(100) {}
 
 Datatable::~Datatable() {}
 
+// Missing asm instructions in saveWebsite
 void Datatable::saveWebsite(char *content, char *userdata) {
-	unsigned int a = HashFunction(content, this, this->c);
+	unsigned int a = HashFunction(content, this, this->max_transfer_size_in_bytes);
     printf("%s|||-%d-|||\n", content, (unsigned long) a);
-    // strcpy((char *)(this + (long)(this->websites_saved * this->d) + 0x10), content); // The asm instr adds a random (char *)(our_calc) * 0x10 in param 1
-    //                                                                                  // of strcpy which must be removed
+    // Instructions below segfault
+    // strcpy((char *)(this + (long)(this->websites_saved * this->d) + 0x10), content);
     // strcpy((char*)(this + a), userdata);
     this->websites_saved += 1;
 }
 
-int HashFunction(char *content, Datatable *datatable, int param_3) {
-    return datatable->websites_saved * datatable->c;
+int HashFunction(char *content, Datatable *datatable, int max_transfer_size_in_bytes) {
+    return datatable->websites_saved * datatable->max_transfer_size_in_bytes;
 }
 
 #endif DATATABLE_H
