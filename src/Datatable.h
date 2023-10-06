ac0d11ffd3f58fd4e9ae3eaea3d08b81ffa6e872
Jack Boller <jackboller1@gmail.com>
2023-10-05 19:25:44 -0500

    switch saved_websites to num_requests_made

--- src/Datatable.h
+++ src/Datatable.h
@@ -8,7 +8,7 @@ public:
     Datatable();
     char url[0x190000];
     int a;
-    int websites_saved;
+    int num_requests_made;
     int c;
     int d;
     void saveWebsite(char *content, char *userdata);
@@ -23,7 +23,7 @@ Datatable::Datatable() {
     *(int *)(this + 0x19000c) = 100;
     */
     a = 100;
-    websites_saved = 0;
+    num_requests_made = 0;
     c = 0x4000;
     d = 100;
 }
@@ -35,17 +35,17 @@ Datatable::~Datatable() {
 void Datatable::saveWebsite(char *content, char *userdata) {
 	unsigned int a = HashFunction(content, this, this->c);
     printf("%s|||-%d-|||\n", this->url, (unsigned long) a);
-    // strcpy((char *)(this + (long)(this->websites_saved * this->d) + 0x10), content); // The asm instr adds a random (char *)(our_calc) * 0x10 in param 1
+    // strcpy((char *)(this + (long)(this->num_requests_made * this->d) + 0x10), content); // The asm instr adds a random (char *)(our_calc) * 0x10 in param 1
     //                                                                                  // of strcpy which must be removed
     // strcpy((char*)(this + a), userdata);
-    this->websites_saved += 1;
+    this->num_requests_made += 1;
 }
 
 int HashFunction(char *content, Datatable *datatable, int param_3) {
-    // printf("Websites saved %d\n", datatable->websites_saved);
+    // printf("Websites saved %d\n", datatable->num_requests_made);
     // printf("c %d\n", datatable->c);
     // printf("should be %d\n", 0x4000);
-    return datatable->websites_saved * datatable->c;
+    return datatable->num_requests_made * datatable->c;
 }
 
 #endif DATATABLE_H
